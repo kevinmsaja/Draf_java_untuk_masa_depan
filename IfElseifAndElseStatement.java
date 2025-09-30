4367094ca55d41714b6f92d3fe3b7f49a6a6a489
Kevin Maulana S <kevin.maulana.sempri@gmail.com>
2025-09-30 11:12:47 +0700

    Update IfElseifAndElseStatement.java

--- IfElseifAndElseStatement.java
+++ IfElseifAndElseStatement.java
@@ -5,19 +5,19 @@ public class Nilai {
 		// TODO Auto-generated method stub
 		int nilai;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Inputkan nilai angka");
+		System.out.print("Berapa nilaimu? = ");
 		nilai=input.nextInt();
 		input.close();
-		if(nilai >= 81) {
-		    System.out.println("A");
+		if (nilai >= 81) {
+		    System.out.println("Kamu A ya...");
 		} else if (nilai >= 70) {
-			System.out.println("B");
-		}	else if (nilai >= 60) {
-			System.out.println("C");
-		}	else if (nilai >= 50) {
-			System.out.println("D");
-		}	else {
-			System.out.println("E");	
+			System.out.println("Kamu B ya...");
+		} else if (nilai >= 60) {
+			System.out.println("Kamu C ya...");
+		} else if (nilai >= 50) {
+			System.out.println("Yah, dapat D. Bete gw jir..");
+		} else {
+			System.out.println("Sorry ye, dapat E...");	
 		}
 	}
 }
